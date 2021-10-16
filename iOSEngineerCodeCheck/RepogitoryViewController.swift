cb895bc518b90c5382d33eea24888c178b7464e3
ShunMc <news_two_three@yahoo.co.jp>
2021-10-16 15:10:34 +0900

    一部フィールド、変数名の修正と、if letをguard letに置き換え

--- iOSEngineerCodeCheck/RepogitoryViewController.swift
+++ iOSEngineerCodeCheck/RepogitoryViewController.swift
@@ -21,39 +21,39 @@ class RepogitoryViewController: UIViewController {
     @IBOutlet weak var forksCountLabel: UILabel!
     @IBOutlet weak var issuesCountLabel: UILabel!
     
-    var vc1: SearchViewController!
-        
+    var searchVC: SearchViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let repogitory = vc1.repogitories[vc1.selectedIndex]
+        let repo = searchVC.repogitories[searchVC.selectedIndex]
         
-        languageLabel.text = "Written in \(repogitory["language"] as? String ?? "")"
-        starsCountLabel.text = "\(repogitory["stargazers_count"] as? Int ?? 0) stars"
-        watchersCountLabel.text = "\(repogitory["wachers_count"] as? Int ?? 0) watchers"
-        forksCountLabel.text = "\(repogitory["forks_count"] as? Int ?? 0) forks"
-        issuesCountLabel.text = "\(repogitory["open_issues_count"] as? Int ?? 0) open issues"
+        languageLabel.text = "Written in \(repo["language"] as? String ?? "")"
+        starsCountLabel.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
+        watchersCountLabel.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
+        forksCountLabel.text = "\(repo["forks_count"] as? Int ?? 0) forks"
+        issuesCountLabel.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
         getImage()
-        
     }
     
-    func getImage(){
+    func getImage() {
+        let repo = searchVC.repogitories[searchVC.selectedIndex]
         
-        let repogitory = vc1.repogitories[vc1.selectedIndex]
+        repogitoryNameLabel.text = repo["full_name"] as? String
         
-        repogitoryNameLabel.text = repogitory["full_name"] as? String
-        
-        if let owner = repogitory["owner"] as? [String: Any] {
-            if let imgURL = owner["avatar_url"] as? String {
-                URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, res, err) in
-                    let img = UIImage(data: data!)!
-                    DispatchQueue.main.async {
-                        self.thumbnailImageView.image = img
-                    }
-                }.resume()
-            }
+        guard let owner = repo["owner"] as? [String: Any] else {
+            return
+        }
+        guard let imgURL = owner["avatar_url"] as? String else {
+            return
         }
         
+        URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, res, err) in
+            let img = UIImage(data: data!)!
+            DispatchQueue.main.async {
+                self.thumbnailImageView.image = img
+            }
+        }.resume()
     }
     
 }
--- iOSEngineerCodeCheck/SearchViewController.swift
+++ iOSEngineerCodeCheck/SearchViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SearchViewController: UITableViewController, UISearchBarDelegate {
-
+    
     @IBOutlet weak var searchBar: UISearchBar!
     
     var repogitories: [[String: Any]]=[]
@@ -37,34 +37,34 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
         searchWord = searchBar.text!
         
-        if searchWord.count != 0 {
-            url = "https://api.github.com/search/repositories?q=\(searchWord!)"
-            task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
-                if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
-                    if let items = obj["items"] as? [[String: Any]] {
-                    self.repogitories = items
-                        DispatchQueue.main.async {
-                            self.tableView.reloadData()
-                        }
-                    }
-                }
-            }
-        // これ呼ばなきゃリストが更新されません
-        task?.resume()
+        if searchWord.count == 0 {
+            return;
         }
         
+        url = "https://api.github.com/search/repositories?q=\(searchWord!)"
+        task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
+            guard let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] else {
+                return
+            }
+            guard let items = obj["items"] as? [[String: Any]] else {
+                return
+            }
+            self.repogitories = items
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        // これ呼ばなきゃリストが更新されません
+        task?.resume()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         if segue.identifier == "Detail"{
-            let dtl = segue.destination as! RepogitoryViewController
-            dtl.vc1 = self
+            let dst = segue.destination as! RepogitoryViewController
+            dst.searchVC = self
         }
-        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -72,21 +72,18 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = UITableViewCell()
-        let rp = repogitories[indexPath.row]
-        cell.textLabel?.text = rp["full_name"] as? String ?? ""
-        cell.detailTextLabel?.text = rp["language"] as? String ?? ""
+        let repo = repogitories[indexPath.row]
+        cell.textLabel?.text = repo["full_name"] as? String ?? ""
+        cell.detailTextLabel?.text = repo["language"] as? String ?? ""
         cell.tag = indexPath.row
         return cell
-        
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 画面遷移時に呼ばれる
         selectedIndex = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
-        
     }
     
 }
